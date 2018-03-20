b307c60e1d44b5f051ff2368a00b923f041b5b59
Samila Ruane <samilaruane@gmail.com>
2018-03-19 21:58:25 -0300

    refactor: Turning event listener more generic

--- app/src/main/java/br/com/samilaruane/carteiravirtual/utils/OnDatabaseAccessListener.kt
+++ app/src/main/java/br/com/samilaruane/carteiravirtual/utils/OnDatabaseAccessListener.kt
@@ -3,6 +3,6 @@ package br.com.samilaruane.carteiravirtual.utils
 /**
  * Created by samila on 14/02/18.
  */
-interface OnDatabaseAccessListener {
-    fun onSelectSuccess (dataList : List <Object>)
+interface OnDatabaseAccessListener <T> {
+    fun onSelectSuccess (obj : T)
 }
