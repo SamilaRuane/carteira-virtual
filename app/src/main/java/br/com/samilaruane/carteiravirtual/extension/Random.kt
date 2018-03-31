968dc0df33d9b1ed46c79cea0377b2f877fa3de5
Samila Ruane <samilaruane@gmail.com>
2018-03-31 18:55:14 -0300

    fix: adding a class that was missing on repository

---
+++ app/src/main/java/br/com/samilaruane/carteiravirtual/extension/Random.kt
@@ -0,0 +1,13 @@
+package br.com.samilaruane.carteiravirtual.extension
+
+import java.util.*
+
+/**
+ * Created by samila on 26/03/18.
+ */
+fun Random.generateToken () : String{
+    val randomicNumber = this.nextInt(9999 - 1000) + 1000
+    val token = randomicNumber.toString()
+
+    return token
+}
